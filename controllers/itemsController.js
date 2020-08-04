0c9482daa6ec315309d5d1a4675107069cb339bc
Darryl Grissom <GrissomDarrylE@JohnDeere>
2020-08-04 09:37:43 -0500

    working on routes and views, just saving

---
+++ controllers/itemsController.js
@@ -0,0 +1,77 @@
+//CREATE controlers
+const express = require("express");
+const item = require("../models/item");
+const router = express.Router();
+
+// Add pokemon model
+// const pokemon = require("../pokemon.js");
+//Sequelized
+const ItemModel = require("../models").Item;
+const UserModel = require("../models").User;
+
+
+//Sequelized - INDEX route
+router.get("/", (req, res) => {
+  ItemModel.findAll().then((itemsAllFromDb) => {
+    res.render("index.ejs", {item: itemsAllFromDb});
+  })
+});
+
+//Sequelized - CREATE via POST ROUTE --> controllers
+router.post("/", (req, res)=>{
+  ItemModel.create(req.body).then(() => {
+    res.redirect("/item");
+  });
+});
+
+//UPDATE function needs both EDIT & PUT routes
+//Sequelized -  UPDATE -- > //EDIT route
+router.get('/:id/edit', function(req, res){
+	ItemModel.findByPk(req.params.id).then((itemToEdit) => {
+    res.render('edit.ejs', {
+      item: itemToEdit,
+    });
+  });
+});
+//Sequelized - UPDATE - //PUT route
+router.put('/:id', (req, res) => {
+  ItemModel.update(req.body, 
+    {where: {id: req.params.id}, returning: true,}).then((itemUpdated) => {
+    res.redirect("/item"); //redirect to the index page
+  });
+});
+
+//NEW ROUTE - SEND EMPTY FORM
+//put this above your show.ejs file
+//Sequelized - no changes needed
+router.get('/new', (req, res) => {
+    res.render('new.ejs');
+  });
+  
+//Sequelized -  SHOW ROUTE with join
+router.get('/:id', (req, res) => {
+    ItemModel.findByPk(req.params.id, {
+        include : [UserModel],
+    })
+    .then((item) => {
+        console.log(item);
+        res.render("show.ejs", {
+            item: item
+        });
+    })
+});
+  
+//DELETE 
+// router.delete('/:id', (req, res) => {
+//       pokemon.splice(req.params.index, 1); //remove the item from the array
+//       res.redirect('/pokemon');  //redirect back to index route
+// });
+
+//Sequelized -  DELETE  (note Sequelized method is '.destroy')
+router.delete('/:id', (req, res) => {
+  ItemModel.destroy({ where: { id: req.params.id } }).then(() => {
+    res.redirect("/item");
+  });
+});
+
+module.exports = router;
